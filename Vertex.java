87ae6988796ae68d92508c5457ed5d25dece739b
RialJohnson <31868183+RialJohnson@users.noreply.github.com>
2017-11-16 11:01:17 -0700

    Create Vertex.java
    A Vertex object that contains details about the graph's nodes.

---
+++ Vertex.java
@@ -0,0 +1,11 @@
+// class from last lab given by Rance
+public class Vertex {
+	public char label; 
+	public boolean wasAdded;
+
+	public Vertex(char lab) 
+	{
+		label = lab;
+		wasAdded = false;
+	}
+}
